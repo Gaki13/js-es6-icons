ee46fce25060297ab4ccd3e21c215010264cdc19
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-27 18:09:29 +0200

    stampo le icon a schermo

--- js/main.js
+++ js/main.js
@@ -138,3 +138,23 @@ const data = [
 
 // Milestone 1
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+const containerHTML = document.querySelector('.container');
+const selectIcon = document.querySelector('#icon-type');
+
+function printIcons(icons, container) {
+    // svuoto il container delle icone
+    container.innerHTML = "";
+    // aggiungo le icone
+    icons.forEach((elm) => {
+        const icon = `
+            <div class="icon">
+                <i class="${elm.family} ${elm.prefix+elm.name}" style="color: ${elm.color}"></i>
+                <div>${elm.name}</div>
+            </div>
+        `;
+        container.innerHTML = container.innerHTML + icon;
+    });
+}
+
+// on load della pagina aggiungo tutte le icone presenti nell'array
+printIcons(icons, containerHTML);
